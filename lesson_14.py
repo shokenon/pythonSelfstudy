5fa3a5e50a335a6b683e2a307cf9d189efbef93c
shokenon <ibnhasan98@gmail.com>
2021-12-18 12:57:41 +0500

    yangi kodlar yozdim
    lug'atlar bilan ishlashni organyapman.

--- lesson_14.py
+++ lesson_14.py
@@ -5,3 +5,46 @@ Created on Thu Dec 16 17:26:16 2021
 @author: user
 """
 
+"""otam = {'ismi': 'xasan', 'tyil': 1955, 'viloyat': ' xorazm'}
+
+
+tyil = otam['tyil']
+
+vil = otam['viloyat']
+
+print(f"Otamning ismi  {otam['ismi'].title()}, {tyil}-yilda, {vil.title()} \
+      viloyatida tug'ilganlar")
+      
+      
+taomlar = {'birinchi': 'osh', 'ikkinchi': 'sho\'rva', 'uchinchi':'halim', \
+           'to\'rtinchi': 'baliq', 'beshinchi': 'manti'
+           }
+    
+sardor = taomlar['birinchi']
+
+ezoza = taomlar['to\'rtinchi']
+
+odilbek = taomlar['beshinchi']
+
+
+print(f"Sardor {sardor} ni yaxshi ko'radi, E'zoza {ezoza} ni yaxshi ko'radi, Odilbek {odilbek}ni yaxshi ko'radi.")
+"""
+
+python_izohli_lugati = {
+    'integer':"Butun son",
+    'float':"O'nlik son",
+    'string':"Matn",
+    'list':"Ro'yxat",
+    'tuple':"O'zgarmas ro'yxat"}
+
+
+
+kalit = input("Kalit so'z kiriting:").lower()
+print(python_izohli_lugati.get(kalit,"Bunday so'z mavjud emas"))
+
+kalit = input("Kalit so'z kiriting:").lower()
+tarjima = python_izohli_lugati.get(kalit)
+if tarjima==None:
+    print("Bunday so'z mavjud emas")
+else:
+    print(f"{kalit.title()} so'zi {tarjima} deb tarjima qilinadi")
