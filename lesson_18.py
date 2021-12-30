63d23c8dc711173e78b114de575313d9de22b193
shokenon <ibnhasan98@gmail.com>
2021-12-30 16:17:21 +0500

    looplar boyicha yangi ma'lumotlar o'rgandim
    while for looplar tugadi. Endi functionlarni organish maqsad :D

--- lesson_18.py
+++ lesson_18.py
@@ -5,15 +5,85 @@ Created on Tue Dec 28 12:29:20 2021
 @author: user
 """
 
-savol = "Kiritilgan sonning ildizini qaytaruvchi dastur. \n"
-savol += "Musbat son kiriting "
-savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# savol = "Kiritilgan sonning ildizini qaytaruvchi dastur. \n"
+# savol += "Musbat son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# while True:
+#     qiymat = int(input(savol))
+#     if qiymat < 0:
+#         continue
+#     elif qiymat == "exit":
+#         break
+#     else:
+#         ildiz = float(qiymat)**(0.5)
+#         print(f"{qiymat} ning ildizi {ildiz} ga teng")
+
+# ismlar= []
+
+# print("Yaqin do'stlaringizn ro'yxatini tuzamiz.")
+# n=1
+# while True:
+#     savol = f"{n} - do'stingiz ismini kiriting:"
+#     ism = input(savol)
+#     ismlar.append(ism)
+#     javob = input("Yana ism qo'shasizmni? (ha/yo'q) ")
+#     if javob == 'ha':
+#         n+=1
+#         continue
+#     else:
+#         break
+# print("Ro'yxat tuzildi") 
+# print("Do'stlaringiz ro'yxati:")
+# for ism in ismlar:
+#     print(ism.title())
+    
+
+
+# print("do'stingiz yoshini saqlaymiz.")
+# dostlar = {}
+
+# ishora = True
+# while ishora:
+#     ism = input("dostingiz ismini kiriting: ")
+#     yosh = input(f"{ism.title()}ning yoshini kiriting:")
+#     dostlar[ism] = int(yosh)
+    
+#     javob = input("yana ma'lumot qo'shasizmi? (ha/yo'q')")
+#     if javob == "yo'q":
+#         ishora = False 
+# for ism , yosh in dostlar.items():
+#     print(f"{ism.title()} {yosh} yoshda") 
+    
+
+# cars = ['lacetti', 'nexia', 'toyota', 'nexia', 'malibu', 'nexia']
+# while 'nexia' in cars :
+#     cars.remove('nexia')
+# print(cars)
+
+
+taomlar = []
+
+print("Taom buyuramiz")
+n=1
 while True:
-    qiymat = int(input(savol))
-    if qiymat < 0:
+    savol = f"{n} - taomni yozing:"
+    nomi = input(savol)
+    taomlar.append(nomi)
+    javob = input("Yana taom buyurasizmi? (ha/yo'q) ")
+    if javob == 'ha':
+        n+=1
         continue
-    elif qiymat == "exit":
-        break
     else:
-        ildiz = float(qiymat)**(0.5)
-        print(f"{qiymat} ning ildizi {ildiz} ga teng")
+        break
+print("Ro'yxat tuzildi") 
+print("Do'stlaringiz ro'yxati:")
+for nomi in taomlar:
+    print(nomi.title())
+    
+
+
+    
+    
+    
+    
+    
